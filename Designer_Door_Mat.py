afeca23f5ae1a9ff8b94cdb157b733856eebfe9c
alkendrasingh <61915219+alkendrasingh@users.noreply.github.com>
2020-04-27 22:33:48 +0530

    Add files via upload

---
+++ Designer_Door_Mat.py
@@ -0,0 +1,11 @@
+n, m = map(int,input().split())
+
+for i in range(0,n//2):
+    x='.|.'*(i*2+1)
+    print(x.center(m,'-'))
+    
+    
+print("WELCOME".center(m,'-'))
+for i in range(n//2-1,-1,-1):
+    x='.|.'*(i*2+1)
+    print(x.center(m,'-'))
